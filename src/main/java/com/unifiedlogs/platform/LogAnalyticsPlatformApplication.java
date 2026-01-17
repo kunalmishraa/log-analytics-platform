458691e3a476f068b9934d96b31eee983701e310
kunal.mishra <mishrakunal012@gmail.com>
2026-01-17 12:53:21 +0530

    initial commit

---
+++ src/main/java/com/unifiedlogs/platform/LogAnalyticsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.unifiedlogs.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyticsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyticsPlatformApplication.class, args);
+	}
+
+}
