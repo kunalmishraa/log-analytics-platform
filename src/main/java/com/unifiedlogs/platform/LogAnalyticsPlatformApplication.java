2e78b4c9ac30ab7e5b9ccdc916c1f1b3a42c9556
kunal.mishra <mishrakunal012@gmail.com>
2026-01-17 13:22:27 +0530

    added a comment

--- src/main/java/com/unifiedlogs/platform/LogAnalyticsPlatformApplication.java
+++ src/main/java/com/unifiedlogs/platform/LogAnalyticsPlatformApplication.java
@@ -1,4 +1,10 @@
 package com.unifiedlogs.platform;
+/**
+ * Author : Kunal Mishra
+ * Date : 17-01-2026
+ */
+
+/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
